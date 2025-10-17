3734617d23c48193d09d4d4e4d44b129c290975f
Carpediem2684 <yannick.teta@gmail.com>
2025-10-17 14:32:10 +0200

    app.py

--- app.py
+++ app.py
@@ -60,9 +60,9 @@ else:
     # KPI en ligne
     st.markdown(f"<p style='text-align:right; font-size:14px;'>Date du jour : {date_du_jour}</p>", unsafe_allow_html=True)
     col1, col2, col3, col4 = st.columns(4)
-    col1.metric("PIC Réalisé", f"{pic_realise[mois_selectionne]} m²")
-    col2.metric("PIC Prévu", f"{pic_prevu[mois_selectionne]} m²")
-    col3.metric("Ruptures", f"{ruptures}")
+    col1.metric("PIC Réalisé", f"{pic_realise[mois_selectionne]} km²")
+    col2.metric("PIC Prévu", f"{pic_prevu[mois_selectionne]} km²")
+    col3.metric("Ruptures cette semaine", f"{ruptures}")
     col4.metric("Adhérence S-1", f"{taux_adherence}%")
 
     # Graphiques en grille
