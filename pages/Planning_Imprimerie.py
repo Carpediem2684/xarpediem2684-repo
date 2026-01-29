e477257c9336951266ca56479d8e2dd16249ec9b
Carpediem2684 <yannick.teta@gmail.com>
2026-01-29 19:33:14 +0100

    Planning_Imprimerie.py

--- pages/Planning_Imprimerie.py
+++ pages/Planning_Imprimerie.py
@@ -1,10 +1,10 @@
 import streamlit as st
+
 def show_planning_Imprimerie():
-    st.title("🖨️ Planning Imprimerie – UAP 4M")
+    st.title("🏭 Planning Ligne Imprimerie – UAP 4M")
     st.write("Page en cours de construction...")
 
     st.write("")
 
     if st.button("⬅️ Retour au menu principal"):
         st.session_state["page"] = "menu"
-
