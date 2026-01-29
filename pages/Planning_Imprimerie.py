146a2fd3c5feea8075e374a59acd203ed80b12f1
Carpediem2684 <yannick.teta@gmail.com>
2026-01-29 19:32:26 +0100

    Planning_Imprimerie.py

--- pages/Planning_Imprimerie.py
+++ pages/Planning_Imprimerie.py
@@ -3,11 +3,8 @@ def show_planning_Imprimerie():
     st.title("🖨️ Planning Imprimerie – UAP 4M")
     st.write("Page en cours de construction...")
 
-    
-st.write("")  # petite marge
+    st.write("")
 
-    # Bouton retour menu
     if st.button("⬅️ Retour au menu principal"):
-        # On change la page dans la session
         st.session_state["page"] = "menu"
 
