d43330a789067e64bfda695aeb51cc27c5477931
Carpediem2684 <yannick.teta@gmail.com>
2026-01-29 19:29:03 +0100

    Planning_Visitage.py

--- pages/Planning_Visitage.py
+++ pages/Planning_Visitage.py
@@ -2,3 +2,11 @@ import streamlit as st
 def show_planning_Visitage():
     st.title("🔍 Planning Visitage – UAP 4M")
     st.write("Page en cours de construction...")
+    
+st.write("")  # petite marge
+
+    # Bouton retour menu
+    if st.button("⬅️ Retour au menu principal"):
+        # On change la page dans la session
+        st.session_state["page"] = "menu"
+
