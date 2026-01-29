0fb9c4096f30670f66cbb6e6663fd73e75122c25
Carpediem2684 <yannick.teta@gmail.com>
2026-01-29 14:49:49 +0100

    Menu.py

--- pages/Menu.py
+++ pages/Menu.py
@@ -10,45 +10,34 @@ def show_menu():
     """, unsafe_allow_html=True)
 
     st.write("")
-
-    if uap != "4M":
-        st.info("Le menu détaillé est pour l'instant disponible uniquement pour l'UAP 4M.")
-        st.write("---")
-
     st.subheader("Choisir un module :")
 
     col1, col2 = st.columns(2)
     col3, col4 = st.columns(2)
     col5, _ = st.columns(2)
 
-    # Dashboard PIC
+    # ---- BOUTONS ----
+
     with col1:
-        if st.button("📊 Dashboard PIC"):
-    st.markdown("<meta http-equiv='refresh' content='0; url=./Dashboard_PIC'/>", unsafe_allow_html=True)
+        if st.button("📊 Dashboard PIC", use_container_width=True):
+            st.session_state["page"] = "dashboard_pic"
 
-    # Planning L1
     with col2:
-        if st.button("🏭 Planning Ligne 1"):
-    st.markdown("<meta http-equiv='refresh' content='0; url=./Planning_L1'/>", unsafe_allow_html=True)
+        if st.button("🏭 Planning Ligne 1", use_container_width=True):
+            st.session_state["page"] = "planning_l1"
 
-
-    # Planning Imprimerie
     with col3:
-        if st.button("🖨️ Planning Imprimerie"):
-    st.markdown("<meta http-equiv='refresh' content='0; url=./Planning_Imprimerie'/>", unsafe_allow_html=True)
+        if st.button("🖨️ Planning Imprimerie", use_container_width=True):
+            st.session_state["page"] = "planning_imprimerie"
 
-    # Planning Ligne 2
     with col4:
-        if st.button("🖨️ Planning Ligne 2"):
-    st.markdown("<meta http-equiv='refresh' content='0; url=./Planning_L2'/>", unsafe_allow_html=True)
+        if st.button("⚙️ Planning Ligne 2", use_container_width=True):
+            st.session_state["page"] = "planning_l2"
 
-    # Planning Visitage
     with col5:
-        if st.button("🖨️ Planning Imprimerie"):
-    st.markdown("<meta http-equiv='refresh' content='0; url=./Planning_Visitage'/>", unsafe_allow_html=True)
+        if st.button("🔍 Planning Visitage", use_container_width=True):
+            st.session_state["page"] = "planning_visitage"
 
     st.write("")
     if st.button("⬅️ Retour à l'accueil", use_container_width=True):
         st.session_state["page"] = "home"
-        st.experimental_rerun()
-``
