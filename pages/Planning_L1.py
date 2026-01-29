21b0cae3085af6a0d72aea33bac8a39b67f1cd68
Carpediem2684 <yannick.teta@gmail.com>
2026-01-29 19:13:04 +0100

    Planning_L1.py

--- pages/Planning_L1.py
+++ pages/Planning_L1.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.title("🏭 Planning Ligne 1 – UAP 4M")
-st.write("Page en cours de construction...")
+def show_planning_l1():
+    st.title("🏭 Planning Ligne 1 – UAP 4M")
+    st.write("Page en cours de construction...")
