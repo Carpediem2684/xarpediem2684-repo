6b3dee688cd694a01dc2e30c8bc06c4ac5640527
Carpediem2684 <yannick.teta@gmail.com>
2026-01-29 19:31:26 +0100

    Planning_L1.py

--- pages/Planning_L1.py
+++ pages/Planning_L1.py
@@ -3,11 +3,9 @@ import streamlit as st
 def show_planning_l1():
     st.title("🏭 Planning Ligne 1 – UAP 4M")
     st.write("Page en cours de construction...")
-    
-st.write("")  # petite marge
 
-    # Bouton retour menu
+    st.write("")
+
     if st.button("⬅️ Retour au menu principal"):
-        # On change la page dans la session
         st.session_state["page"] = "menu"
 
