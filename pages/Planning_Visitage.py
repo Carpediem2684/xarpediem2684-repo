c211dd06b20daa36ecdc949f2426771860eae164
Carpediem2684 <yannick.teta@gmail.com>
2026-01-30 07:45:34 +0100

    Planning_Visitage.py

--- pages/Planning_Visitage.py
+++ pages/Planning_Visitage.py
@@ -1,6 +1,6 @@
 import streamlit as st
 def show_planning_Visitage():
-    st.title("🔍 Planning Visitage – UAP 4M")
+    st.title("🔍 Planning visitage – UAP 4M")
     st.write("Page en cours de construction...")
 
     st.write("")
