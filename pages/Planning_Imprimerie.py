fed83deb9f151ea2c6e39fdbf7342de578ddd7ec
Carpediem2684 <yannick.teta@gmail.com>
2026-01-30 07:43:50 +0100

    Update Planning_Imprimerie.py

--- pages/Planning_Imprimerie.py
+++ pages/Planning_Imprimerie.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def show_Planning_Imprimerie():
+def show_planning_imprimerie():
     st.title("🏭 Planning Ligne Imprimerie – UAP 4M")
     st.write("Page en cours de construction...")
 
